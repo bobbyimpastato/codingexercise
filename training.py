3d68ed9b42b9c35f3c8202f292848ad3ade74055
bobbyimpastato <rdi4217@nyu.edu>
2024-09-27 19:28:17 -0400

    added search parameter functionality

--- training.py
+++ training.py
@@ -5,6 +5,49 @@ import time
 import json
 from datetime import datetime
 
+trainings_input = ["Electrical Safety for Labs", "X-Ray Safety", "Laboratory Safety Training"]
+
+fiscal_year_input = 2024
+while True:
+    print("Default fiscal year:", fiscal_year_input)
+    choice = input("Would you like to use default fiscal year? [Y/N]: ")
+    if choice == "N" or choice == "n":
+        try:
+            fiscal_year_input = int(input("Custom Fiscal year: "))
+        except:
+            print("ERROR: Please enter a number")
+        break
+    elif choice == "Y" or choice == "y":
+        break
+    else:
+        print("ERROR: Enter Y or N")
+
+while True:
+    print("Default trainings parameters:", trainings_input)
+    choice = input("Would you like to use default training parameters? [Y/N]: ")
+    if choice == "N" or choice == "n":
+        newlist = []
+        try:
+
+            num_parameters = int(input("How many training parameters?: "))
+        except:
+            print("ERROR: Please enter a number")
+            continue
+
+        for i in range(0,num_parameters):
+            parameter = input(f"Parameter {i+1}: ")
+            newlist.append(parameter)
+        print("New parameters:", newlist)
+        trainings_input = newlist
+
+        break
+       
+    elif choice == "Y" or choice == "y":
+        break
+    else:
+        print("ERROR: Enter Y or N")
+
+
 #name of file (edit if necessary)
 filename = "trainings (correct).txt"
 
@@ -12,14 +55,14 @@ filename = "trainings (correct).txt"
 try:
     myfile = open(filename, 'r')
     data = json.load(myfile)
-    print("Opened file")
+    print(f"Opened file {filename}")
 except:
     print("ERROR OPENING FILE! (Program assumes file is in same repository and named 'trainings (correct).txt'")
 
 
 #print data
 #time.sleep(1)
-print(json.dumps(data, indent=4))
+#print(json.dumps(data, indent=4))
 
 
 completed_counts = {}  #store counts
@@ -52,3 +95,42 @@ for person in data:
 print("\nNumber of people who completed each training:\n")
 for name in completed_counts:
     print("\t" + name + ": " + str(completed_counts[name]),"\n")
+
+
+fiscal_year_start = datetime(fiscal_year_input - 1, 7, 1) #7/1/23
+fiscal_year_end = datetime(fiscal_year_input, 6, 30) #6/30/24
+
+#init empty dictionary
+people_completetrainings = {}
+
+#iterate through all relevant trainings and add entry to dict
+for training in trainings_input:
+    people_completetrainings[training] = []
+
+#once again iterate through each item (person) in the data
+for person in data:
+    for completion in person["completions"]:
+        training_name = completion["name"]
+        timestamp = completion["timestamp"]
+        completion_date = datetime.strptime(timestamp, "%m/%d/%Y")
+        
+        #check if current training matches target list
+        if training_name in trainings_input:
+            #check if within target fiscal year
+            if fiscal_year_start <= completion_date <= fiscal_year_end:
+                people_completetrainings[training_name].append(person["name"])
+#print results
+print("\nSpecified trainings:",end="")
+for item in trainings_input:
+    print(item + ", ", end="")
+print(f"\nPeople who completed specified trainings in FY {fiscal_year_input}:\n")
+
+for training, people in people_completetrainings.items():
+    print(f"{training}:")
+    if people:  #check if empty
+        for person in people:
+            print(f"      {person}")
+    else:
+        print("      No one completed this training.")  #nobody completed
+    
+    print("")
